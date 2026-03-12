570ccd6749829c561278074116a183b3a52a74a4
Teofil Bodea <teo.bodea12@yahoo.com>
2026-03-12 12:13:09 +0000

    Add mlnx_ofed to all-packages.nix

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1195,6 +1195,8 @@ with pkgs;
 
   bump2version = with python3Packages; toPythonApplication bump2version;
 
+  mlnx_ofed = callPackage ../by-name/ml/mlnx_ofed { kernel = linuxPackages.kernel; };
+
   cgit = callPackage ../applications/version-management/cgit { };
 
   cgit-pink = callPackage ../applications/version-management/cgit/pink.nix { };
