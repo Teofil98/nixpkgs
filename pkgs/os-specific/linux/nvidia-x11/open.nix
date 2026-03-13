fc7ff5f41673ceae8bb4e2150e9d293c5545d0da
Teofil Bodea <teo.bodea12@yahoo.com>
2026-03-13 10:08:14 +0000

    More patches to open.nix

--- pkgs/os-specific/linux/nvidia-x11/open.nix
+++ pkgs/os-specific/linux/nvidia-x11/open.nix
@@ -36,14 +36,32 @@ stdenv.mkDerivation {
       --replace-fail "/usr/src/ofa_kernel" \
       "${mlnx_ofed}/lib/modules/${kernel.modDirVersion}/extra/mlnx-ofa_kernel"
 
-  sed -i 's|check_for_ib_peer_memory_symbols "\$MLNX_OFED_KERNEL_DIR/\$ARCH/\$KERNELRELEASE" |check_for_ib_peer_memory_symbols "\$MLNX_OFED_KERNEL_DIR" |' \
-  kernel-open/conftest.sh
-sed -i '/check_for_ib_peer_memory_symbols "\$MLNX_OFED_KERNEL_DIR\/\$KERNELRELEASE"/d' \
-  kernel-open/conftest.sh
-sed -i '/check_for_ib_peer_memory_symbols "\$MLNX_OFED_KERNEL_DIR\/default"/d' \
-  kernel-open/conftest.sh
-sed -i '/check_for_ib_peer_memory_symbols "\$VAR_DKMS_SOURCES_DIR"/d' \
-  kernel-open/conftest.sh
+
+    substituteInPlace kernel-open/conftest.sh \
+  --replace-fail \
+  "if check_for_ib_peer_memory_symbols \"\$OUTPUT\" || \\" \
+  "if check_for_ib_peer_memory_symbols \"\$OUTPUT\" || check_for_ib_peer_memory_symbols \"\$MLNX_OFED_KERNEL_DIR\"; then"
+
+substituteInPlace kernel-open/conftest.sh \
+  --replace-fail \
+  "           check_for_ib_peer_memory_symbols \"\$MLNX_OFED_KERNEL_DIR/\$ARCH/\$KERNELRELEASE\" || \\" \
+  ""
+
+substituteInPlace kernel-open/conftest.sh \
+  --replace-fail \
+  "           check_for_ib_peer_memory_symbols \"\$MLNX_OFED_KERNEL_DIR/\$KERNELRELEASE\" || \\" \
+  ""
+
+substituteInPlace kernel-open/conftest.sh \
+  --replace-fail \
+  "           check_for_ib_peer_memory_symbols \"\$MLNX_OFED_KERNEL_DIR/default\" || \\" \
+  ""
+
+substituteInPlace kernel-open/conftest.sh \
+  --replace-fail \
+  "           check_for_ib_peer_memory_symbols \"\$VAR_DKMS_SOURCES_DIR\"; then" \
+  ""
+
 
     #substituteInPlace kernel-open/conftest.sh \
     #  --replace-fail "/usr/src/ofa_kernel" \
